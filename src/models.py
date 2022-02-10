8f53558c96c5b027dea93bf9b3ae0a42850f7700
Luis Piñero Quintana <lpineroq@gmail.com>
2022-02-10 13:50:47 +0000

    Todo_list_API_python_flask

--- src/models.py
+++ src/models.py
@@ -2,18 +2,20 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Todo(db.Model):
+    __tablename__ = 'todo'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    todo = db.Column(db.String(120), nullable=False)
+    status = db.Column(db.Boolean, default=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
-
+        return '<Todo %r>' % self.id
+    
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "email": self.todo,
+            "status": self.status
         }
+
+    
